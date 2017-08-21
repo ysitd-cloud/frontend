647bd740b732e7653f47c04ddc36d0e962590064
Tony Yip <tony@opensource.hk>
2017-08-21 16:47:56 +0800

    Fix assets

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const serveStatic = require('express-static-gzip');
 const app = express();
 
 app.use(morgan('common'));
-app.use('/assets', serveStatic('/dist'));
+app.use('/assets', serveStatic('dist'));
 
 app.listen(process.env.PORT);
