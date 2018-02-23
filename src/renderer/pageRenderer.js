5cc63b293926b608321660bc5767dd729959fede
Tony Yip <tony@opensource.hk>
2018-02-23 15:32:19 +0800

    Extract code for better abstraction

--- src/renderer/pageRenderer.js
+++ src/renderer/pageRenderer.js
@@ -2,22 +2,24 @@ const { Environment, FileSystemLoader } = require('nunjucks');
 const hypernova = require('hypernova');
 const { DEBUG } = require('../env');
 
+function createRenderFn(template) {
+  return props => new Promise((resolve, reject) => {
+    template.render(props, (e, content) => {
+      if (e) {
+        reject(e);
+      } else {
+        resolve(content);
+      }
+    });
+  });
+}
+
 function render(name, component) {
   return hypernova({
     server() {
       const env = new Environment(new FileSystemLoader('view', { watch: DEBUG }));
       const template = env.getTemplate(component);
-      return function renderPage(props) {
-        return new Promise((resolve, reject) => {
-          template.render(props, (e, content) => {
-            if (e) {
-              reject(e);
-            } else {
-              resolve(content);
-            }
-          });
-        });
-      };
+      return createRenderFn(template);
     },
     client() {},
   });
