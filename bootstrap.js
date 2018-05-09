0af69231caebba54a8f2ca2451b86e5b9432dcd0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-09 03:41:35 +0000

    Deploy assets.ysitd.cloud to github.com/ysitd-cloud/frontend.git:gh-pages

---
+++ bootstrap.js
@@ -0,0 +1,2 @@
+define(["vue","vuetify"],(e,i)=>{e.use(i,{theme:{primary:"#44A148",secondary:"#303F9F"}})});
+//# sourceMappingURL=bootstrap.js.map
---
+++ jspm.config.js
@@ -0,0 +1,2 @@
+System.config({defaultJSExtensions:!1,transpiler:!1,meta:{"*.css":{loader:"css.js"},"*.js":{scriptLoad:!0}},map:{vue:"https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.16/vue.runtime.js",vuetify:"https://cdnjs.cloudflare.com/ajax/libs/vuetify/1.0.17/vuetify.min.js"}});
+//# sourceMappingURL=jspm.config.js.map
